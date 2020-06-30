32ecf0e2ecedd09a43a0c0e3981c0f881c9f5b73
clydeqin7 <clydeqin7@foxmail.com>
2020-06-30 21:03:43 +0800

    feat: px2rem引入，style常量引入

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,26 @@
+const px2rem = require("postcss-plugin-px2rem");
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/resume/" : "/"
+  publicPath: process.env.NODE_ENV === "production" ? "/resume/" : "/",
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          px2rem({
+            // postcss-plugin-px2rem 默认配置
+            rootValue: 100,
+            unitPrecision: 5,
+            propWhiteList: [],
+            propBlackList: [],
+            exclude: /node_modules/,
+            selectorBlackList: [],
+            ignoreIdentifier: false,
+            replace: true,
+            mediaQuery: false,
+            minPixelValue: 0
+          })
+        ]
+      }
+    }
+  }
 };
